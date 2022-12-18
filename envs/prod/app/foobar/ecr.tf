7b9b8b62124e63e91ba0424f07e2884182e2e682
y.maeda <yuyuyu.mae606@gmail.com>
2022-12-18 23:07:48 +0900

    ecrのリソース定義

---
+++ envs/prod/app/foobar/ecr.tf
@@ -0,0 +1,7 @@
+resource "aws_ecr_repository" "nginx" {
+  name = "example-prod-foobar-nginx"
+  tags = {
+    Name = "example-prod-foobar-nginx"
+  }
+  
+}
--- envs/prod/app/foobar/provider.tf
+++ envs/prod/app/foobar/provider.tf
@@ -1,12 +1,1 @@
-provider "aws" {
-  region = "ap-northeast-1"
-}
-
-terraform {
-  required_provider {
-    aws = {
-      source = "hashicorp/aws"
-      version = "3.42.0"
-    }
-  }
-}
+../../provider.tf
