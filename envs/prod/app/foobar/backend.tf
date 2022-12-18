5f7ffba6b938cf11d361331d4954f147a14fda5c
y.maeda <yuyuyu.mae606@gmail.com>
2022-12-18 10:38:29 +0900

    s3のバケット作成用の.tfファイイル作成

---
+++ envs/prod/app/foobar/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "01-laravel-fargate-app-tfstate" # S3に作成したバケット名を記載
+    key = "example/prod/app/foobar_1.0.0.tfstate"  # 例としてexampleをシステム名/prod/app/foobar_terraformのバージョン.tfstate
+    region = "ap-northeast-1" # 東京リージョンを指定
+  }
+}
