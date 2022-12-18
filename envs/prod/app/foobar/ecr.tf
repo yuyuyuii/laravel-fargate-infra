89fd28940ec74ea5717785af43231ac4fcee7e05
y.maeda <yuyuyu.mae606@gmail.com>
2022-12-18 23:30:29 +0900

    ecrのライフサイクルポリシー作成

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -3,5 +3,27 @@ resource "aws_ecr_repository" "nginx" {
   tags = {
     Name = "example-prod-foobar-nginx"
   }
-  
+}
+
+# ecrは最新かそれに近いイメージを使うので、古いイメージは自動削除するようにライフサイクルを定義する 
+resource "aws_ecr_lifecycle_policy" "nginx" {
+  policy = jsonencode(
+    {
+      "rules" : [
+        {
+          "rulePriority": 1,
+          "description": "Hold only 10 image, expire all others",
+          "selection": {
+            "tagStatus": "any",
+            "countType": "imageCountMoreThan",
+            "countNumber": 10
+          },
+          "action": {
+            "type": "expire"
+          }
+        }
+      ]
+    }
+  )
+  repository = aws_ecr_repository.nginx.name
 }
