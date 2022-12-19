d618e85ab380dbaac7fb15298e02db54898377c1
y.maeda <yuyuyu.mae606@gmail.com>
2022-12-19 22:03:19 +0900

    feat:ecr.tfのモジュール化に伴い不要コード削除

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -1,37 +1,5 @@
-resource "aws_ecr_repository" "nginx" {
-  name = "example-prod-foobar-nginx"
-  tags = {
-    Name = "example-prod-foobar-nginx"
-  }
-}
-
-# ecrは最新かそれに近いイメージを使うので、古いイメージは自動削除するようにライフサイクルを定義する 
-resource "aws_ecr_lifecycle_policy" "nginx" {
-  policy = jsonencode(
-    {
-      "rules" : [
-        {
-          "rulePriority": 1,
-          "description": "Hold only 10 image, expire all others",
-          "selection": {
-            "tagStatus": "any",
-            "countType": "imageCountMoreThan",
-            "countNumber": 10
-          },
-          "action": {
-            "type": "expire"
-          }
-        }
-      ]
-    }
-  )
-  repository = aws_ecr_repository.nginx.name
-}
-
 # モジュール呼び出し
 module "nginx" {
   source = "../../../../modules/ecr"
   name = "example-prod-foobar-nginx"
-
-  
 }
