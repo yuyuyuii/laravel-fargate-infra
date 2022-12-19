4d2663378f7645bb635bb00474692aab54a7a84d
y.maeda <yuyuyu.mae606@gmail.com>
2022-12-19 22:10:19 +0900

    feat:php用のモジュール作成。変数とかはまだ定義してない

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -3,3 +3,9 @@ module "nginx" {
   source = "../../../../modules/ecr"
   name = "example-prod-foobar-nginx"
 }
+
+# PHP用のモジュール作成
+module "php" {
+  source = "../../../../modules/ecr/"
+  name = "example-prod-foobar-php"
+}
