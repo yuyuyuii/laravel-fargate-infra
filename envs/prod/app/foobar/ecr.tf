26937400f2f9204aaaf33f71aeee5343c12973a6
y.maeda <yuyuyu.mae606@gmail.com>
2022-12-19 22:22:26 +0900

    feat:システム名と環境名を共通化し、各ファイルを修正

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -1,11 +1,11 @@
 # モジュール呼び出し
 module "nginx" {
   source = "../../../../modules/ecr"
-  name = "example-prod-foobar-nginx"
+  name = "${local.name_prefix}-${local.service_name}-nginx"
 }
 
 # PHP用のモジュール作成
 module "php" {
   source = "../../../../modules/ecr/"
-  name = "example-prod-foobar-php"
+  name = "${local.name_prefix}-${local.service_name}-php"
 }
